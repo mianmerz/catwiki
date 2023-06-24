be002069f5b303da0ec5fdccae84d13afb6d0e2b
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-06-24 20:04:14 -0300

    feat: fix align info banner

--- src/pages/home/components/Banner/Banner.jsx
+++ src/pages/home/components/Banner/Banner.jsx
@@ -8,6 +8,7 @@ export const Banner = () => {
 
         <section className="info px-5">
           <Logo color="white" width={315} height={104} />
+
           <h5 className="subtitle">Get to know more about your cat breed</h5>
         </section>
 
