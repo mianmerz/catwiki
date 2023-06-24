9e913efcc8f2f010c1c9289131f83f813c6dd3ce
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-06-24 18:13:04 -0300

    add bootstrap and sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.scss'
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
