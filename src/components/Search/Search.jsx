a755a86084c7cfff9b17c9bacb1609a150c7bf45
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-06-24 20:59:20 -0300

    feat: add style of search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,11 @@
+import './Search.scss';
+
 export const Search = () => {
   return (
     <section className="search-component">
 
-      <div className="input-group mb-3">
-        <input type="text" className="form-control-lg" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
         <span className="input-group-text" id="basic-addon2">B</span>
       </div>
 
