1f80d0dceea6675cefb18c62139efff94b821e82
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-06-24 20:50:24 -0300

    ref: add Search component

--- src/pages/home/components/Banner/Banner.jsx
+++ src/pages/home/components/Banner/Banner.jsx
@@ -1,15 +1,17 @@
- import { Logo } from "../../../../components";
+ import { Logo, Search } from "../../../../components";
 import './Banner.scss';
 
 export const Banner = () => {
   return (
-    <section className="banner container-fluid px-5" >
+    <section className="banner-component container-fluid px-5" >
       <div className="content p-5">
 
         <section className="info px-5">
           <Logo color="white" width={315} height={104} />
 
           <h5 className="subtitle">Get to know more about your cat breed</h5>
+
+          <Search />
         </section>
 
       </div>
