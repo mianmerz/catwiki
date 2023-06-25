e82bce27cd808f7b3a86e4930edb4a2ec41e154e
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-06-25 14:50:35 -0300

    feat: implement icons and seeMore

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Banner } from "./components/Banner/Banner"
+import { BoxBreeds } from "./components/BoxBreeds/BoxBreeds"
 
 export const HomePage = () => {
   return (
     <main>
       <Banner />
+      <BoxBreeds />
     </main>
   )
 }
---
+++ src/pages/home/components/BoxBreeds/BoxBreeds.jsx
@@ -0,0 +1,32 @@
+import './BoxBreads.scss';
+
+export const BoxBreeds = () => {
+    return (
+        <section className="box-breads-component container-fluid px-5" >
+            <div className="content p-5">
+
+                <div className="row">
+                    <div className="col title">
+                        <h6>Most Searched Breeds</h6>
+                        <div className="line"></div>
+                    </div>
+                </div>
+
+
+                <div className="row align-items-end">
+                    <div className="col py-5">
+                        <h1 className="text-start title ">66+ Breeds For you to discover</h1>
+                    </div>
+
+                    <div className="col py-5 d-flex justify-content-end" >
+                        <button type="button" className="btn btn-link d-flex align-items-end gap-2">
+                            <span>SEE MORE</span>
+                            <span className="material-symbols-outlined">trending_flat</span>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
